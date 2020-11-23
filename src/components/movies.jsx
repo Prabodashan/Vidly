ea41b4e5561f8f8214d71756afce6a23760c98dc
Prabodashan <prabodashan14@gmail.com>
2020-11-23 22:29:01 +0530

    implementing sorting

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,17 +5,19 @@ import Pagination from './common/pagination';
 import { getMovies } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService';
 import { paginate } from '../utils/paginate';
+import _ from 'lodash';
 
 class Movies extends Component {
     state = {
         movies: [],
         genres: [],
         currentPage: 1,
-        pageSize: 4
+        pageSize: 4,
+        sortColumn: { path: 'title', order: 'asc' }
     };
 
     componentDidMount() {
-        const genres = [{ name: 'All Genres' }, ...getGenres()];
+        const genres = [{ _id: "", name: 'All Genres' }, ...getGenres()];
 
         this.setState({ movies: getMovies(), genres });
     };
@@ -41,11 +43,23 @@ class Movies extends Component {
         this.setState({ selectedGenre: genre, currentPage: 1 });
     };
 
+    handleSort = path => {
+        const sortColumn = { ...this.state.sortColumn };
+        if (sortColumn.path === path)
+            sortColumn.order = (sortColumn.order === 'asc') ? 'desc' : 'asc';
+        else {
+            sortColumn.path = path;
+            sortColumn.order = 'asc';
+        }
+        this.setState({ sortColumn });
+    };
+
     render() {
         const { length: count } = this.state.movies;
         const {
             pageSize,
             currentPage,
+            sortColumn,
             selectedGenre,
             movies: allMovies
         } = this.state;
@@ -57,7 +71,9 @@ class Movies extends Component {
                 ? allMovies.filter(m => m.genre._id === selectedGenre._id)
                 : allMovies;
 
-        const movies = paginate(filtered, currentPage, pageSize);
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+        const movies = paginate(sorted, currentPage, pageSize);
 
         return (
             <div className="row">
@@ -73,7 +89,9 @@ class Movies extends Component {
                     <MoviesTable
                         movies={movies}
                         onLike={this.handleLike}
-                        onDelete={this.onDelete} />
+                        onDelete={this.onDelete}
+                        onSort={this.handleSort}
+                    />
                     <Pagination
                         itemsCount={filtered.length}
                         pageSize={pageSize}
